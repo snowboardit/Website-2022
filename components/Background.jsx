1073cf50b148736d5db35263ae303f9ea0877dd4
Max Lareau <max@maxlareau.com>
2022-02-19 11:26:49 -0500

    install tailwind and get it working on index

---
+++ components/Background.jsx
@@ -0,0 +1,9 @@
+function Background() {
+
+    return (
+        <section className="flex w-screen h-screen m-0 bg-slate-400">
+        </section>
+    );
+}
+
+export default Background;
---
+++ components/Hero.jsx
@@ -0,0 +1,7 @@
+function Hero() {
+    return (
+        <h1 className="text-3xl font-bold underline">Background</h1>
+    );
+}
+
+export default Hero;
