86f2bd7ef78d9ff25c6695a3ec26eb9c8e071673
Max Lareau <snowboardit@me.com>
2022-03-09 16:58:06 -0500

    Begin to make the About section play nice on mobile

--- components/About/About.jsx
+++ components/About/About.jsx
@@ -5,10 +5,10 @@ import CircuitBG from './CircuitBG';
 function About() {
 
     return (
-        <section className="flex h-screen w-full justify-evenly items-center bg-maxLightGray">
+        <section className="flex h-auto min-h-screen lg:h-screen w-full justify-evenly items-center bg-maxLightGray">
 
             {/* LEFT PANEL */}
-            <div className="relative flex h-full w-1/2 justify-center items-center border-r-[1px] border-black/20">
+            <div className="relative flex h-full w-full lg:w-1/2 justify-center items-center border-r-[1px] border-black/20">
 
                 <CircuitBG />
 
@@ -19,7 +19,7 @@ function About() {
 
 
             {/* RIGHT PANEL */}
-            <div className="flex flex-col h-screen w-1/2 justify-center items-center mx-16">
+            <div className="flex flex-col h-screen w-full lg:w-1/2 justify-center items-center mx-16">
 
                 <RightPanel />
 
