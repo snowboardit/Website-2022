7cb1046cd8cb7fbd4f29086cc34120ab25359dcf
CosmWatch <cosmwatch@gmail.com>
2023-01-08 01:35:11 -0500

    Fix build errors

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,25 @@
 module.exports = {
+	settings: {
+		react: {
+			version: 'detect',
+		},
+	},
 	env: {
 		browser: true,
 		es2021: true,
 		node: true,
 	},
-	extends: ['plugin:prettier/recommended'],
-	overrides: [],
+	extends: ['eslint:recommended', 'plugin:next/recommended', 'prettier'],
 	parserOptions: {
-		ecmaVersion: 'latest',
+		ecmaFeatures: {
+			jsx: true,
+		},
+		ecmaVersion: 12,
 		sourceType: 'module',
 	},
-	rules: {},
+	plugins: ['react'],
+	rules: {
+		'react/prop-types': 'off',
+		'react/react-in-jsx-scope': 'off',
+	},
 };
