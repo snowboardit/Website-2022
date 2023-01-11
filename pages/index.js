0b5e7e800092a89a61fb86994f30a7584d6d1bba
Max <max@maxlareau.com>
2023-01-11 03:53:04 -0500

    Update head and favicon set

--- pages/index.js
+++ pages/index.js
@@ -20,17 +20,56 @@ const client = createClient({
 
 export default function Home({ projects }) {
 	return (
-		<main className={`flex flex-col w-full`}>
-			<Background />
+		<>
+			<Head>
+				<title>ML</title>
+				<meta name="description" content="Max's Website" />
+				<meta
+					name="viewport"
+					content="width=device-width, initial-scale=1.0"
+				/>
+				<meta
+					http-equiv="Content-Type"
+					content="text/html; charset=utf-8"
+				/>
+				<link
+					rel="apple-touch-icon"
+					sizes="76x76"
+					href="/apple-touch-icon.png"
+				></link>
+				<link
+					rel="icon"
+					type="image/png"
+					sizes="32x32"
+					href="/favicon-32x32.png"
+				></link>
+				<link
+					rel="icon"
+					type="image/png"
+					sizes="16x16"
+					href="/favicon-16x16.png"
+				></link>
+				<link rel="manifest" href="/site.webmanifest"></link>
+				<link
+					rel="mask-icon"
+					href="/safari-pinned-tab.svg"
+					color="#5bbad5"
+				></link>
+				<meta name="msapplication-TileColor" content="#da532c"></meta>
+				<meta name="theme-color" content="#ffffff"></meta>
+			</Head>
+			<main className={`flex flex-col w-full`}>
+				<Background />
 
-			<About />
+				<About />
 
-			<LogoCarousel />
+				<LogoCarousel />
 
-			<Portfolio projects={projects} />
+				<Portfolio projects={projects} />
 
-			<Contact />
-		</main>
+				<Contact />
+			</main>
+		</>
 	);
 }
 
